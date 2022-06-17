f69b170281d3926b91b81335830e3f9312bbd41d
jackline chepngeno <chepngenojackline0@gmail.com>
2022-06-17 14:20:43 +0300

    complete assignment

--- index.js
+++ index.js
@@ -2,6 +2,48 @@ function isPalindrome(word) {
   // Write your algorithm here
 }
 
+function isPalindrome(word) {
+  // iterate from the beginning of the string to the middle of the string
+  for (let i = 0; i < word.length / 2; i++) {
+      // compare the letter we're iterating over to the corresponding letter at the end of the string
+      const j = word.length - 1 - i;
+      if (word[i] !== word[j]) {
+          // if the letters don't match, return false
+          return false;
+      }
+  }
+
+  // if we reach the middle, and all the letters match, return true
+  return true;
+}
+
+function isPalindrome(word) {
+  // iterate from the beginning of the string to the middle of the string
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+      // compare the letter we're iterating over to the corresponding letter at the end of the string
+      const endIndex = word.length - 1 - startIndex;
+      if (word[startIndex] !== word[endIndex]) {
+          // if the letters don't match, return false
+          return false;
+      }
+  }
+
+  // if we reach the middle, and all the letters match, return true
+  return true;
+}
+
+
+function isPalindrome(word) {
+  for (let startIndex = 0; startIndex < word.length / 2; startIndex++) {
+      const endIndex = word.length - 1 - startIndex;
+      if (word[startIndex] !== word[endIndex]) {
+          return false;
+      }
+  }
+
+  return true;
+}
+
 /* 
   Add your pseudocode here
 */
@@ -16,10 +58,48 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
+  // add your own custom tests in here
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"));
+
+
   console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
 }
 
 module.exports = isPalindrome;
+
+if (require.main === module) {
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
+}
